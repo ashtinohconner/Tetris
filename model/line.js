4ff2dec34ed4d154041aca4709f5a29726d2fdd5
Mike <Budge@Mikes-MacBook-Pro.local>
2016-03-29 15:10:47 -0600

    added to line.js

--- model/line.js
+++ model/line.js
@@ -6,5 +6,24 @@
 
 class Line extends Shape
 {
+	constructor(rotation)
+	{
+		super(0, red)		//......
+		this.rotation = rotation	//	
+		this.positions = [
+				[
+					[0,0,1,0],
+					[0,0,1,0],
+					[0,0,1,0],
+					[0,0,1,0]
+				],
 
+				[
+					[0,0,0,0],
+					[1,1,1,1],
+					[0,0,0,0],
+					[0,0,0,0]
+				]
+			]	// 3d array of all possible positions a given shape can be in at any given time
+	}
 }
