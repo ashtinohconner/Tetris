1273f2fedf355e6a0959e16181da4bd061a104f9
Mike <Budge@facstaf85.comm.utah.edu>
2016-03-30 16:08:40 -0600

    commented out a bunch of code in shape.js

--- model/shape.js
+++ model/shape.js
@@ -22,167 +22,167 @@ class Shape
 		// this.points[3] = this.point3
 	}
 	
-	//	TODO: need to consider left edge in subclasses
-	moveLeft()
-	{
-		if(touchingLeftEdge == true)
-		{
-			//.....
-			return
-		}
-		else
-		{
-			this.point0.x--
-			this.point1.x--
-			this.point2.x--
-			this.point3.x--
-			return
-		}
-	}
+	// //	TODO: need to consider left edge in subclasses
+	// moveLeft()
+	// {
+	// 	if(touchingLeftEdge == true)
+	// 	{
+	// 		//.....
+	// 		return
+	// 	}
+	// 	else
+	// 	{
+	// 		this.point0.x--
+	// 		this.point1.x--
+	// 		this.point2.x--
+	// 		this.point3.x--
+	// 		return
+	// 	}
+	// }
 
-	//	TODO: need to consider right edge in subclasses
-	moveRight()
-	{
-		if(touchingRightEdge() == true)
-		{
-			//.....
-			return
-		}
-		else
-		{
-			this.point0.x++
-			this.point1.x++
-			this.point2.x++
-			this.point3.x++
-			return
-		}
-	}
+	// //	TODO: need to consider right edge in subclasses
+	// moveRight()
+	// {
+	// 	if(touchingRightEdge() == true)
+	// 	{
+	// 		//.....
+	// 		return
+	// 	}
+	// 	else
+	// 	{
+	// 		this.point0.x++
+	// 		this.point1.x++
+	// 		this.point2.x++
+	// 		this.point3.x++
+	// 		return
+	// 	}
+	// }
 
-	//	TODO: need to consider bottom edge in subclasses
-	moveDown()
-	{
-		if(touchingBottomEdge())
-		{
-			//.......
-			return
-		}
-		else if(bottomOut())
-		{
-			//....
-			return
-		}
-		else
-		{
-			this.point0.y++
-			this.point1.y++
-			this.point2.y++
-			this.point3.y++
-			return
-		}
-	}
+	// //	TODO: need to consider bottom edge in subclasses
+	// moveDown()
+	// {
+	// 	if(touchingBottomEdge())
+	// 	{
+	// 		//.......
+	// 		return
+	// 	}
+	// 	else if(bottomOut())
+	// 	{
+	// 		//....
+	// 		return
+	// 	}
+	// 	else
+	// 	{
+	// 		this.point0.y++
+	// 		this.point1.y++
+	// 		this.point2.y++
+	// 		this.point3.y++
+	// 		return
+	// 	}
+	// }
 
-	//	TODO: need to decide if the the only time an edge will affect a rotation is if the piece is in direct contact with it.
-	rotateCounter()
-	{
-		if(touchingLeftEdge())
-		{
-			//...
-			return
-		}
-		else if(touchingRightEdge())
-		{
-			//...
-			return
-		}
-		else if(touchingBottomEdge())
-		{
-			//...
-			return
-		}
-		else if(bottomOut())
-		{
-			//...
-			return
-		}
-		// else no edge intrusion
-		else
-		{
-			//...
-			return
-		}
-	}
+	// //	TODO: need to decide if the the only time an edge will affect a rotation is if the piece is in direct contact with it.
+	// rotateCounter()
+	// {
+	// 	if(touchingLeftEdge())
+	// 	{
+	// 		//...
+	// 		return
+	// 	}
+	// 	else if(touchingRightEdge())
+	// 	{
+	// 		//...
+	// 		return
+	// 	}
+	// 	else if(touchingBottomEdge())
+	// 	{
+	// 		//...
+	// 		return
+	// 	}
+	// 	else if(bottomOut())
+	// 	{
+	// 		//...
+	// 		return
+	// 	}
+	// 	// else no edge intrusion
+	// 	else
+	// 	{
+	// 		//...
+	// 		return
+	// 	}
+	// }
 
-	//	TODO: need to decide if the the only time an edge will affect a rotation is if the piece is in direct contact with it.
-	rotateClock()
-	{
-		if(touchingLeftEdge())
-		{
-			//...
-			return
-		}
-		else if(touchingRightEdge())
-		{
-			//...
-			return
-		}
-		else if(touchingBottomEdge())
-		{
-			//...
-			return
-		}
-		else if(bottomOut())
-		{
-			//...
-			return
-		}
-		// else no edge intrusion
-		else
-		{
-			//...
-			return
-		}
-	}
+	// //	TODO: need to decide if the the only time an edge will affect a rotation is if the piece is in direct contact with it.
+	// rotateClock()
+	// {
+	// 	if(touchingLeftEdge())
+	// 	{
+	// 		//...
+	// 		return
+	// 	}
+	// 	else if(touchingRightEdge())
+	// 	{
+	// 		//...
+	// 		return
+	// 	}
+	// 	else if(touchingBottomEdge())
+	// 	{
+	// 		//...
+	// 		return
+	// 	}
+	// 	else if(bottomOut())
+	// 	{
+	// 		//...
+	// 		return
+	// 	}
+	// 	// else no edge intrusion
+	// 	else
+	// 	{
+	// 		//...
+	// 		return
+	// 	}
+	// }
 
-	touchingLeftEdge()		
-	{
-		for(pos_le = 0; pos_le < 3; pos_le++)
-		{
-			if(points[pos_le].x == 0)
-			{
-				return true
-			}
-		}
+	// touchingLeftEdge()		
+	// {
+	// 	for(pos_le = 0; pos_le < 3; pos_le++)
+	// 	{
+	// 		if(points[pos_le].x == 0)
+	// 		{
+	// 			return true
+	// 		}
+	// 	}
 
-		return false
-	}
+	// 	return false
+	// }
 
-	touchingRightEdge()		
-	{
-		for(pos_re = 0; pos_re < 3; pos_re++)
-		{
-			if(points[pos_re].x == 9)
-			{
-				return true
-			}
-		}
+	// touchingRightEdge()		
+	// {
+	// 	for(pos_re = 0; pos_re < 3; pos_re++)
+	// 	{
+	// 		if(points[pos_re].x == 9)
+	// 		{
+	// 			return true
+	// 		}
+	// 	}
 
-		return false
-	}
+	// 	return false
+	// }
 
-	touchingBottomEdge()		
-	{
-		for(pos_be = 0; pos_be < 3; pos_be++)
-		{
-			if(points[pos_be].y == 19)
-			{
-				return true
-			}
-		}
+	// touchingBottomEdge()		
+	// {
+	// 	for(pos_be = 0; pos_be < 3; pos_be++)
+	// 	{
+	// 		if(points[pos_be].y == 19)
+	// 		{
+	// 			return true
+	// 		}
+	// 	}
 
-		return false	
-	}
+	// 	return false	
+	// }
 
-	//	return true/false
-	bottomOut()				// ??
-	{}
+	// //	return true/false
+	// bottomOut()				// ??
+	// {}
 }
