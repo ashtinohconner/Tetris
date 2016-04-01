82b8c6a1f64d43d8081510c79cdd662108faea41
Mike <Budge@Mikes-MacBook-Pro.local>
2016-04-01 17:37:40 -0600

    commented out obsolete files

--- model/zag2.js
+++ model/zag2.js
@@ -4,24 +4,24 @@
 	3/28/2016
 */
 
-class Zag2 extends Zag1
-{
-	constructor(rotation)
-	{
-		super(6, orange)		//......
-		this.rotation = rotation % 2
-		this.positions = [
-				[
-					[0,0,1],
-					[0,1,1],
-					[0,1,0]
-				],
+// class Zag2 extends Zag1
+// {
+// 	constructor(rotation)
+// 	{
+// 		super(6, orange)		//......
+// 		this.rotation = rotation % 2
+// 		this.positions = [
+// 				[
+// 					[0,0,1],
+// 					[0,1,1],
+// 					[0,1,0]
+// 				],
 
-				[
-					[1,1,0],
-					[0,1,1],
-					[0,0,0]
-				]				
-			]	// 3d array of all possible positions a given shape can be in at any given time
-	}
-}
+// 				[
+// 					[1,1,0],
+// 					[0,1,1],
+// 					[0,0,0]
+// 				]				
+// 			]	// 3d array of all possible positions a given shape can be in at any given time
+// 	}
+// }
